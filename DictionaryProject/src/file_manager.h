0226a373940769ff126e9d268f025ff338fb6988
megazid <mega0gx@gmail.com>
2026-01-31 22:01:58 +0200

    clean+surprise

--- DictionaryProject/src/file_manager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include "dictionary.h"
-
-class FileManager {
-public:
-    static void load(const std::string& path, Dictionary& dict);
-    static void save(const std::string& path, const Dictionary& dict);
-};
